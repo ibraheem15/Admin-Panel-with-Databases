ad5f4d3071c3e2df0c86346b0cf7a260aeb8135e
Ibrahee Rehman <i211102@nu.edu.pk>
2023-06-21 15:41:54 +0500

    Added Signup Page

--- pages/login.js
+++ pages/login.js
@@ -1,31 +1,31 @@
-import React,{useState,useEffect} from "react";
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
 
 async function loginUser(credentials) {
-  return fetch('http://localhost:8080/login', {
-    method: 'POST',
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(credentials)
-  })
-    .then(data => data.json())
- }
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
-export default function Login({setToken}) {
+export default function Login({ setToken }) {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
-  
-  const handleSubmit = async e => {
-    if(username === undefined || password === undefined) return alert("Please fill in all fields");
+  const handleSubmit = async (e) => {
+    if (username === undefined || password === undefined)
+      return alert("Please fill in all fields");
     e.preventDefault();
     const token = await loginUser({
       username,
-      password
+      password,
     });
     setToken(token);
-  }
+  };
 
   return (
     <div className="login-wrapper">
@@ -33,20 +33,36 @@ export default function Login({setToken}) {
       <form>
         <label>
           <p>Username</p>
-          <input type="text" onChange={e => setUsername(e.target.value)} required/>
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} required/>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </label>
         <div>
-          <button type="submit" onClick={handleSubmit}>Submit</button>
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
       </form>
+      <div className="login-wrapper">
+        <Link href="/signup">
+          <a>Sign Up</a>
+        </Link>
+      </div>
     </div>
+    // sign up page
   );
 }
 
 Login.propTypes = {
-  setToken: PropTypes.func.isRequired
-}
+  setToken: PropTypes.func.isRequired,
+};
---
+++ pages/signup.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+function signup() {
+  const [data, setData] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost/api/user/save",
+        data
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value });
+  };
+
+  return (
+    <div className="login-wrapper">
+      <h1 className="">Please Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" required onChange={handleChange} name="username" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            required
+            onChange={handleChange}
+            name="password"
+          />
+        </label>
+        <label>
+          <p>Mobile</p>
+          <input type="text" required onChange={handleChange} name="mobile" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default signup;
